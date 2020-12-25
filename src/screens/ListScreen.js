9676b1387273f3da2ae269bda194f6224c5e16f9
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-12-24 22:15:48 -0500

    ListScreen component

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ListScreen = () => {
+  return <Text style={styles.text}>List Screen component!</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default ListScreen;
