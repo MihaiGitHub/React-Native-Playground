b664aa002a9639d29f3ddf4a55205b98382cf2d6
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-12-26 21:39:40 -0500

    Display other object property

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,12 +3,12 @@ import { Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend 1" },
-    { name: "Friend 2" },
-    { name: "Friend 3" },
-    { name: "Friend 4" },
-    { name: "Friend 5" },
-    { name: "Friend 6" },
+    { name: "Friend 1", age: 20 },
+    { name: "Friend 2", age: 22 },
+    { name: "Friend 3", age: 23 },
+    { name: "Friend 4", age: 24 },
+    { name: "Friend 5", age: 25 },
+    { name: "Friend 6", age: 28 },
   ];
 
   return (
@@ -17,15 +17,19 @@ const ListScreen = () => {
       data={friends}
       renderItem={(element) => {
         // element === { item: { name: "Friend 1" }, index: 0 }
-        return <Text>{element.item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>
+            {element.item.name} - Age {element.item.age}
+          </Text>
+        );
       }}
     />
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
+  textStyle: {
+    marginVertical: 50,
   },
 });
 
