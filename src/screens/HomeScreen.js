f1d64b7effb8d371725495c8f166361f4d1aad9e
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-12-27 22:12:18 -0500

    Add buttons to HomeScreen component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        onPress={() => console.log("pressed")}
+        title="Go to components demo"
+      />
+
+      <TouchableOpacity>
+        <Text>Go to components demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
