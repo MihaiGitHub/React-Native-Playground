d1768038920304ade7d6280d302e990a0913d7d5
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-01-01 22:07:47 -0500

    Create counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,8 +1,17 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
-  return <Text style={styles.textStyle}>This is the counter screen</Text>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <View>
+      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
+      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
+      <Text style={styles.textStyle}>This is the counter screen</Text>
+      {counter}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
